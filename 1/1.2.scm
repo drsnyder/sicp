353b08e8c7fdcb212f17476896f94ad55b088245
Damon Snyder <drsnyder@gmail.com>
2011-08-19 12:10:35 -0700

    oops accidently deleted 1.2

--- 1/1.2.scm
+++ 1/1.2.scm
@@ -126,4 +126,46 @@
           ((= c 0) 1) ; base
           (else (+ (pt (- r 1) (- c 1)) (pt (- r 1) c)))))
         
+
+
+(define (expt b n)
+  (if (= n 0)
+    1
+    (* b (expt b (- n 1)))))
+
+(define (expti b n)
+    (expt-iter b n 1))
+
+(define (expt-iter b counter product)
+  (if (= counter 0)
+    product
+    (expt-iter b
+               (- counter 1)
+               (* b product)))) 
+
+
+(define (square x) (* x x))
+
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+
+; a = 1 : 1
+; a = 2 : 2*2
+
+
+; 1.16
+(define (fast-expt-iter b n a)
+  (cond ((= n 0) a)
+        ((even? n) (fast-expt-iter (square b) (/ n 2) a))
+        (else (fast-expt-iter b (- n 1) (* b a)))))
+
+(define (fast-expti b n)
+    (fast-expt-iter b n 1))
+
     
