7f7125071d006c941bb961b30a7b1067ee24d27a
Damon Snyder <drsnyder@gmail.com>
2011-08-31 13:59:55 -0700

    adding some more 1.2 code and problem solutions

--- 1/1.2.scm
+++ 1/1.2.scm
@@ -201,3 +201,80 @@
         (else (multf-iter a (- b 1) (+ a p)))))
 
 
+; gcd
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+; prime
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (square x) (* x x))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+    (= n (smallest-divisor n)))
+
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+          (remainder (* base (expmod base (- exp 1) m))
+                     m))))        
+
+(define (un-random n)
+  (cond ((or (= n 1) (= n 0)) n)
+        ((= (remainder n 2) 0) (/ n 2))
+        (else (un-random (- n 1)))))
+  
+(define (fermat-test n)
+    (define (try-it a)
+          (= (expmod a n n) a))
+      (try-it (+ 1 (un-random (- n 1)))))
+
+(define (fast-prime? n times)
+    (cond ((= times 0) true)
+          ((fermat-test n) (fast-prime? n (- times 1)))
+          (else false)))
+
+(define (runtime) (current-milliseconds))
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+    (report-prime (- (runtime) start-time))
+    (display " *** no prime ***")))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (find-prime n)
+  (if (even? n)
+    (find-prime (+ 1 n))
+    (if (prime? n) 
+        n
+        (find-prime (+ n 1)))))
+
+(define (find-prime-ft n)
+  (if (even? n)
+    (find-prime (+ 1 n))
+    (if (fermat-test n) 
+        n
+        (find-prime (+ n 1)))))
