4d0074046ad7bb2c850386dc6453231473376a60
Damon Snyder <drsnyder@gmail.com>
2011-11-04 22:05:51 -0700

    adding prob 1.32

--- 1/1.3.scm
+++ 1/1.3.scm
@@ -1,22 +1,60 @@
+; Simpson's Rule http://en.wikipedia.org/wiki/Simpson's_rule
+(define (simpson-integral f a b n)
 
-(define (sum-squares a b) (+ (square a) (square b)))
+  (define (h a b n) 
+    (/ (- b a) n))
 
-(define (sum-squares-max a b c)
+  (define (integrate h)
+
+      (define (fx k)
+
+        (define (y a k h)
+          (+ a (* k h)))
+
+        (cond ((even? k) (* 2 (f (y a k h))))
+              (else (* 4 (f (y a k h))))))
+
+      (/ (* h (sum fx 0 inc n)) 3)) 
+
+  (integrate (h a b n)))
+
+; 1.30 iterative sum
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+      result
+      (iter (next a) (+ result (term a)))))
+  (iter a 0))
+
+(sum identity 0 inc 10)
+
+; 1.31 product
+; TODO: compute pi using these
+(define (product-r term a next b)
   (if (> a b)
-    ; true a > b
-    (if (> b c)
-      (sum-squares a b)
-      (sum-squares a c))
-    ; false b > a
-    (if (> a c)
-      (sum-squares b a)
-      (sum-squares b c))
-    )
-  )
+    1
+    (* (term a)
+       (product-r term (next a) next b))))
 
+(define (product-i term a next b)
+  (define (iter a result)
+    (if (> a b)
+      result
+      (iter (next a) (* result (term a)))))
+  (iter a 1))
+    
 
-(sum-squares-max 4 3 5)
-(sum-squares-max 3 4 5)
-(sum-squares-max 5 4 3)
-(sum-squares-max 5 3 4)
+; 1.32 accumulate
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+    null-value
+    (combiner (term a) 
+              (accumulate combiner null-value term (next a) next b))))
 
+(define (accumulate-i combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a)
+              (combiner result (term a)))))
+  (iter a null-value))
