501b5412af84731e0a9ee12fb5a50b71d3e17434
Damon Snyder <drsnyder@gmail.com>
2011-11-05 15:34:40 -0700

    adding 1.33

--- 1/1.3.scm
+++ 1/1.3.scm
@@ -58,3 +58,50 @@
         (iter (next a)
               (combiner result (term a)))))
   (iter a null-value))
+
+; 1.33 filtered accumulate
+(define (filtered-accumulate combiner null-value term a next b fil)
+  (if (> a b)
+    null-value
+    (if (fil a)
+        (combiner (term a) 
+                  (filtered-accumulate combiner null-value term (next a) next b fil))
+        (filtered-accumulate combiner null-value term (next a) next b fil))))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (square x) (* x x))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+    (= n (smallest-divisor n)))
+
+(define (pass x) #t)
+
+; product 1..5
+(filtered-accumulate * 1 identity 1 inc 5 pass) 
+
+; sum square prime? 1..5
+(filtered-accumulate + 0 square 1 inc 5 prime?)
+(filtered-accumulate + 0 square 2 inc 3 prime?)
+
+
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+
+(define (product-relatively-prime a n)
+    (define (relatively-prime? i)
+      (= (gcd i n) 1))
+    (filtered-accumulate * 1 identity 1 inc n relatively-prime?))
+
