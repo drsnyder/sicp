59adf08fe884bbc23f8f4287472fd885baea2075
Damon Snyder <drsnyder@gmail.com>
2011-11-06 22:04:53 -0800

    adding 1.35

--- 1/1.3.scm
+++ 1/1.3.scm
@@ -105,3 +105,62 @@
       (= (gcd i n) 1))
     (filtered-accumulate * 1 identity 1 inc n relatively-prime?))
 
+; 1.34
+(define (f g)
+    (g 2))
+
+(f square)
+(f (lambda (z) (* z (+ z 1))))
+
+; 1.35
+(define (search f neg-point pos-point)
+  (let ((midpoint (average neg-point pos-point)))
+    (if (close-enough? neg-point pos-point)
+      midpoint
+      (let ((test-value (f midpoint)))
+        (cond ((positive? test-value)
+               (search f neg-point midpoint))
+              ((negative? test-value)
+               (search f midpoint pos-point))
+              (else midpoint))))))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (close-enough? x y)
+    (< (abs (- x y)) 0.001))
+
+(define (half-interval-method f a b)
+  (let ((a-value (f a))
+        (b-value (f b)))
+    (cond ((and (negative? a-value) (positive? b-value))
+           (search f a b))
+          ((and (negative? b-value) (positive? a-value))
+           (search f b a))
+          (else
+            (error "Values are not of opposite sign" a b)))))
+
+;;;
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+        next
+        (try next))))
+  (try first-guess))
+
+
+(fixed-point cos 1.0)
+(fixed-point square 0.2)
+(fixed-point (lambda (y) (+ (sin y) (cos y)))
+                          1.0)
+(define (sqrt x)
+  (fixed-point (lambda (y) (average y (/ x y))) 
+               1.0))
+
+(define (golden x)
+  (fixed-point (lambda (y) (+ 1 (/ 1 y)))
+               1.0))
