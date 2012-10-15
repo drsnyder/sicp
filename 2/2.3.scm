6c77233a68649d39832d376f2beff9b57b4dcecf
Damon Snyder <drsnyder@gmail.com,damon@huddler-inc.com>
2012-10-15 13:08:29 -0700

    adding 2.56

--- 2/2.3.scm
+++ 2/2.3.scm
@@ -69,4 +69,41 @@
         (else (list '* m1 m2))))
 
 
+; 2.56
 
+(define (exponentiation? x)
+  (and (pair? x) (eq? (car x) '**)))
+
+(define (base e)
+  (cadr e))
+
+(define (exponent e)
+  (caddr e))
+
+(define (make-exponentiation b e)
+  (cond ((=number? b 0) 0)
+        ((=number? e 0) 1)
+        ((=number? e 1) b)
+        (else (list '** b e))))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+           (make-product (multiplier exp)
+                         (deriv (multiplicand exp) var))
+           (make-product (deriv (multiplier exp) var)
+                         (multiplicand exp))))
+        ((exponentiation? exp)
+         (make-product
+             (make-product (exponent exp)
+                           (make-exponentiation (base exp)
+                                                (make-sum (exponent exp) -1)))
+             (deriv (base exp) var)))
+        (else
+         (error "unknown expression type -- DERIV" exp))))
