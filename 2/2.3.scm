570e7f871ab4b5ee40f0c28250cb6db4819299e8
Damon Snyder <drsnyder@gmail.com>
2013-02-06 09:04:23 -0800

    up to 2.61

--- 2/2.3.scm
+++ 2/2.3.scm
@@ -122,6 +122,66 @@
 
 ; 2.58
 
+;; sets
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)        
+         (cons (car set1)
+               (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+
+; 2.59
+; recursively, until set1 is empty, union the cdr of set1 to the set resulting from adjoining the car of set1 to
+; set2
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        (else (union-set (cdr set1) 
+                         (adjoin-set (car set1) set2)))))
+
+;; sets as orderd lists
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (car set)) true)
+        ((< x (car set)) false)
+        (else (element-of-set? x (cdr set)))))
+
+(define (intersection-set set1 set2)
+  (if (or (null? set1) (null? set2))
+    '()    
+    (let ((x1 (car set1)) (x2 (car set2)))
+      (cond ((= x1 x2)
+             (cons x1
+                   (intersection-set (cdr set1)
+                                     (cdr set2))))
+            ((< x1 x2)
+             (intersection-set (cdr set1) set2))
+            ((< x2 x1)
+             (intersection-set set1 (cdr set2)))))))
+
+; 2.61
+; adjoin-set
+; if s1 is empty, create a new set with x
+; if x = s1 return s1
+; if x < s1 cons x onto s1
+; if x > s1 cons car s onto adjoin-set x with the cdr of s
+(define (adjoin-set x s)
+  (cond ((null? s) (cons x '()))
+        ((= x (car s)) s)
+        ((< x (car s)) (cons x s))
+        ((> x (car s)) 
+         (cons (car s) 
+               (adjoin-set x (cdr s))))))
 
 
 
