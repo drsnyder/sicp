eca27919e8a9dfc5cbd629e34e83aeee740aa5b7
Damon Snyder <drsnyder@gmail.com>
2013-02-25 08:56:18 -0800

    2.63

--- 2/2.3.scm
+++ 2/2.3.scm
@@ -178,62 +178,119 @@
 (define (adjoin-set x s)
   (cond ((null? s) (cons x '()))
         ((= x (car s)) s)
-        ((< x (car s)) (cons x s))
+        ((< x (car s)) 
+         (cons x s))
         ((> x (car s)) 
          (cons (car s) 
                (adjoin-set x (cdr s))))))
 
+; 2.62
+; union-set
+; correct? tests?
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else (let ((x1 (car set1)) (x2 (car set2)))
+                (cond ((< x1 x2) 
+                       (cons x1 (union-set (cdr set1) set2)))
+                      ((> x1 x2) 
+                       (cons x2 (union-set set1 (cdr set2))))
+                      (else (cons x1 (union-set (cdr set1) (cdr set2)))))))))
 
 
+; sets as tree
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+(define (make-tree entry left right)
+  (list entry left right))
+(define (make-leaf entry)
+  (make-tree entry '() '()))
 
 
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (entry set)) true)
+        ((< x (entry set))
+         (element-of-set? x (left-branch set)))
+        ((> x (entry set))
+         (element-of-set? x (right-branch set)))))
 
 
+(define (adjoin-set x set)
+  (cond ((null? set) (make-tree x '() '()))
+        ((= x (entry set)) set)
+        ((< x (entry set))
+         (make-tree (entry set) 
+                    (adjoin-set x (left-branch set))
+                    (right-branch set)))
+        ((> x (entry set))
+         (make-tree (entry set)
+                    (left-branch set)
+                    (adjoin-set x (right-branch set))))))
+
+; 2.63
+
+(define (rand-seq n)
+  (if (= n 0) 
+    '()
+    (cons (random 100)
+          (rand-seq (- n 1)))))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define s (rand-seq 10))
+(define t (accumulate (lambda (x set) (adjoin-set x set)) '() s))  
+
+; the difference between these two depends on append
+(define (tree->list-1 tree)
+  (if (null? tree)
+      '()
+      (append (tree->list-1 (left-branch tree))
+              (cons (entry tree)
+                    (tree->list-1 (right-branch tree))))))
 
 
+(define (copy-to-list tree result-list)
+  (if (null? tree)
+    result-list
+    (copy-to-list (left-branch tree)
+                  (cons (entry tree)
+                        (copy-to-list (right-branch tree)
+                                      result-list)))))
 
+(define (tree->list-2 tree)
+  (copy-to-list tree '()))
 
+(define t1
+  (make-tree 7
+             (make-tree 3
+                        (make-leaf 1)
+                        (make-leaf 5))
+             (make-tree 9
+                        '()
+                        (make-leaf 11))))
 
+(define t2
+  (make-tree 3
+             (make-leaf 1)
+             (make-tree 7
+                        (make-leaf 5)
+                        (make-tree 9
+                                   '()
+                                   (make-leaf 11)))))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+(define t3
+  (make-tree 5
+             (make-tree 3
+                        (make-leaf 1)
+                        '())
+             (make-tree 9
+                        (make-leaf 7)
+                        (make-leaf 11))))
 
 
